92cf72400022194bb5da8fc2fb07e4e7cb89fc30
Srishti Sengupta <srishti.sengupta@gmail.com>
2015-11-10 16:04:36 +0530

    Added application and client keys for Parse

--- app/src/main/java/stalk/example/com/stalk/ParseApplication.java
+++ app/src/main/java/stalk/example/com/stalk/ParseApplication.java
@@ -9,8 +9,8 @@ import com.parse.ParseInstallation;
  * Created by Srishti on 10/11/2015.
  */
 public class ParseApplication extends Application {
-    public static final String YOUR_APPLICATION_ID = "WU842Ed8GWCo7napgpaxk9FBSZ6LBqrhj6cv0XoO";
-    public static final String YOUR_CLIENT_KEY = "Z5WO1weLaVu7ZAQdn97qEjTApHPoDG0BFM77OUqv";
+    public static final String YOUR_APPLICATION_ID = "pTqcXkZ8goYbNjh73Ds0LCuHH2gOHdFz2kNBWa9F";
+    public static final String YOUR_CLIENT_KEY = "2vmKTHDN4693ejjGYI2ain8QXVhTk7b3tuYnwnb7";
 
     @Override
     public void onCreate() {
